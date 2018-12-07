b9b6e8fcd14a59d1f08d3d6c11859b61d354dc05
Kris Selden <kris.selden@gmail.com>
2018-12-07 12:10:25 -0800

    update stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
-module.exports = {
-  entry: 'dag-map.js',
-  targets: [{
-    dest: 'dag-map.umd.js',
-    format: 'umd',
-    moduleName: 'DAG',
-    moduleId: 'dag-map',
-    sourceMap: true,
-    exports: 'named' // required to be compatible with es2015 import
-                     // in typescript
-  }]
+import sourcemaps from "rollup-plugin-sourcemaps";
+
+export default {
+  input: "dag-map.js",
+  plugins: [sourcemaps()],
+  output: [
+    {
+      file: "dag-map.umd.js",
+      format: "umd",
+      name: "DAG",
+      moduleId: "dag-map",
+      sourcemap: true,
+      exports: "named" // required to be compatible with es2015 import
+      // in typescript
+    }
+  ]
 };
